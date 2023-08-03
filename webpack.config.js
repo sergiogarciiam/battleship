461b2e27ce2f65ef1b5b6147bf350acaf61e0033
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2023-08-03 15:15:57 +0200

    Try fix favicon icon

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Battleship",
-      favicon: "./src/assets/images/favicon.ico",
+      favicon: "src/assets/images/favicon.ico",
     }),
   ],
   output: {
